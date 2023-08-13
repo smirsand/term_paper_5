4a72ba162f1eddb32fedf71e901a0430414034cd
Sergey <smirsand@mail.ru>
2023-08-13 22:23:10 +0300

    add code

--- src/dbmanager.py
+++ src/dbmanager.py
@@ -8,19 +8,19 @@ class DBManager:
 
     def __init__(self):
         params = config()
-        self.conn = psycopg2.connect(database='headhanter', **params)
+        self.conn = psycopg2.connect(database='headhunter', **params)
 
-    def get_companies_and_vacancies_count(self):
+    def get_employers_and_vacancies_count(self):
         """ Получает список всех компаний и количество вакансий у каждой компании."""
 
         with self.conn.cursor() as cur:
             cur.execute(
-                "SELECT company.name_employer, COUNT(vacancies.vacancies_id) as vacancies_count "
-                "FROM company LEFT JOIN vacancies ON company.employer_id = vacancies.employer_id "
-                "GROUP BY company.name_employer")
+                "SELECT employers.name_employer, COUNT(vacancies.vacancies_id) as vacancies_count "
+                "FROM employers LEFT JOIN vacancies ON employers.employer_id = vacancies.employer_id "
+                "GROUP BY employers.name_employer")
             rows = cur.fetchall()
-            for row in rows:
-                print(row)
+
+            return rows
 
     def get_all_vacancies(self):
         """ Получает список всех вакансий с указанием названия компании,
@@ -28,11 +28,11 @@ class DBManager:
 
         with self.conn.cursor() as cur:
             cur.execute(
-                "SELECT vacancies.name_vacancies, company.name_employer, vacancies.salary, vacancies.vacancies_id "
-                "FROM company INNER JOIN vacancies ON company.employer_id = vacancies.employer_id")
+                "SELECT employers.name_employer, vacancies.name_vacancies, vacancies.salary, vacancies.alternate_url "
+                "FROM employers INNER JOIN vacancies ON employers.employer_id = vacancies.employer_id")
             rows = cur.fetchall()
-            for row in rows:
-                print(row)
+
+            return rows
 
     def get_avg_salary(self):
         """ Получает среднюю зарплату по вакансиям."""
@@ -41,8 +41,8 @@ class DBManager:
             cur.execute(
                 "SELECT SUM(vacancies.salary) / COUNT(vacancies.vacancies_id) AS average_salary FROM vacancies")
             rows = cur.fetchall()
-            for row in rows:
-                print(row)
+
+            return rows[0]
 
     def get_vacancies_with_higher_salary(self):
         """ Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
@@ -52,50 +52,48 @@ class DBManager:
                 "SELECT * FROM vacancies WHERE vacancies.salary > (SELECT SUM(salary) / COUNT(vacancies_id) "
                 "FROM vacancies)")
             rows = cur.fetchall()
-            for row in rows:
-                print(row)
+            return rows
 
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, keyword):
         """ Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “java”."""
 
         with self.conn.cursor() as cur:
             cur.execute(
-                "SELECT * FROM vacancies WHERE name_vacancies LIKE '%Python%'")
+                f"SELECT * FROM vacancies WHERE name_vacancies LIKE '%{keyword}%'")
             rows = cur.fetchall()
-            for row in rows:
-                print(row)
+            return rows
 
     def create_table(self):
         """ Создание таблиц для сохранения данных о компаниях и вакансиях."""
 
         with self.conn.cursor() as cur:
             cur.execute("""
-                CREATE TABLE company(
+                CREATE TABLE employers(
                 employer_id SERIAL PRIMARY KEY,
                 name_employer varchar(100) NOT NULL
                 )
                 """)
-            print("OK-1")
 
         with self.conn.cursor() as cur:
             cur.execute("""
                 CREATE TABLE vacancies(
-                employer_id INT REFERENCES company(employer_id),
+                employer_id INT REFERENCES employers(employer_id),
                 vacancies_id int,
                 name_vacancies varchar(100) NOT NULL,
                 salary int,
                 alternate_url varchar NOT NULL
                 )
                 """)
-            print("OK-2")
 
         self.conn.commit()
 
     def drop_table(self):
+        """ Удаление таблиц."""
+
         with self.conn.cursor() as cur:
             cur.execute("""
                 DROP TABLE IF EXISTS vacancies;
-                DROP TABLE IF EXISTS company;
+                DROP TABLE IF EXISTS employers;
                 """)
 
         self.conn.commit()
